86244cfb3c88cf4672727f0837609954a540cb0c
O-Hofmann <hofmannondrej@gmail.com>
2025-12-27 21:12:13 +0100

    Add files via upload

--- script/scriptc.js
+++ script/scriptc.js
@@ -7,3 +7,15 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
 }
+
+
+
+function toggleSubmenu() {
+    const submenu = document.getElementById("fotogalerie-submenu");
+
+    if (submenu.style.display === "block") {
+        submenu.style.display = "none";
+    } else {
+        submenu.style.display = "block";
+    }
+}
